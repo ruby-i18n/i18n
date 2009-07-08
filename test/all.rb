55c1357c6aed90c15c90fc822c7cf206348c4f1d
Sven Fuchs <svenfuchs@artweb-design.de>
2009-07-08 13:25:23 +0200

    just load all the test files ...

--- test/all.rb
+++ test/all.rb
@@ -1,5 +1,3 @@
-dir = File.dirname(__FILE__)
-require dir + '/i18n_test.rb'
-require dir + '/backend/simple/all.rb'
-require dir + '/i18n_exceptions_test.rb'
-# *require* dir + '/custom_backend_test.rb'
+Dir[File.dirname(__FILE__) + '/**/*_test.rb'].each do |file|
+  require file
+end
