10c33452b6831a0261a564ecf8ca625e6948f6a5
Sven Fuchs <svenfuchs@artweb-design.de>
2009-07-14 17:25:43 +0200

    add doc to pluralization module

--- lib/i18n/backend/fallbacks.rb
+++ lib/i18n/backend/fallbacks.rb
@@ -3,16 +3,17 @@ require 'i18n/locale/fallbacks'
 module I18n
   module Backend
     module Fallbacks
-      # Extension of the translate method so that it will try each locale given
-      # by I18n.fallbacks for the given locale. E.g. for the locale :"de-DE" it
-      # might try the locales :"de-DE", :de and :en (depends on the fallbacks
-      # implementation) until it finds a result with the given options. If it
-      # does not find any result for any of the locales it will then raise a
-      # MissingTranslationData exception as usual.
+      # Overwrites the Base backend translate method so that it will try each
+      # locale given by I18n.fallbacks for the given locale. E.g. for the
+      # locale :"de-DE" it might try the locales :"de-DE", :de and :en
+      # (depends on the fallbacks implementation) until it finds a result with
+      # the given options. If it does not find any result for any of the
+      # locales it will then raise a MissingTranslationData exception as
+      # usual.
       #
-      # The default option takes precedence over fallback locales, i.e. it will
-      # first evaluate a given default option before falling back to another
-      # locale.
+      # The default option takes precedence over fallback locales, i.e. it
+      # will first evaluate a given default option before falling back to
+      # another locale.
       def translate(locale, key, options = {})
         I18n.fallbacks[locale].each do |fallback|
           begin
--- lib/i18n/backend/pluralization.rb
+++ lib/i18n/backend/pluralization.rb
@@ -1,9 +1,22 @@
 module I18n
   module Backend
     module Pluralization
+      # Overwrites the Base backend translate method so that it will check the
+      # translation meta data space (:i18n) for locale specific pluralizers
+      # and use them to pluralize the given entry.
+      #
+      # Pluralizers are expected to respond to #call(entry, count) and return
+      # a pluralization key. Valid keys depend on the translation data hash
+      # (entry) but it is generally recommended to follow CLDR's style, i.e.
+      # return one of the keys :zero, :one, :few, :many, :other.
+      #
+      # The :zero key is always picked directly when count equals 0 AND the
+      # translation data has the key :zero. This way translators are free to
+      # either pick a special :zero translation even for languages where the
+      # pluralizer does not return a :zero key.
       def pluralize(locale, entry, count)
         return entry unless entry.is_a?(Hash) and count
-        
+
         pluralizer = pluralizer(locale)
         if pluralizer.respond_to?(:call)
           key = count == 0 && entry.has_key?(:zero) ? :zero : pluralizer.call(count)
@@ -13,9 +26,9 @@ module I18n
           super
         end
       end
-      
+
       protected
-      
+
         def pluralizers
           @pluralizers ||= {}
         end
