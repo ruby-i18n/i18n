fa6a468f28cdd96c83c8c696591e3040af4efec8
Sven Fuchs <svenfuchs@artweb-design.de>
2009-07-19 11:56:56 +0200

    simplify Chain backend implementation by relying on #lookup

--- lib/i18n/backend/chain.rb
+++ lib/i18n/backend/chain.rb
@@ -1,5 +1,19 @@
 module I18n
   module Backend
+    # Backend that chains multiple other backends and checks each of them when
+    # a translation needs to be looked up. This is useful when you want to use
+    # standard translations with a Simple backend but store custom application
+    # translations in a database or other backends.
+    #
+    # To use the Chain backend instantiate it and set it to the I18n module.
+    # You can add chained backends through the initializer or backends 
+    # accessor:
+    #
+    #   # preserves the existing Simple backend set to I18n.backend
+    #   I18n.backend = I18n::Backend::Chain.new(I18n::Backend::ActiveRecord.new, I18n.backend)
+    #
+    # The implementation assumes that all backends added to the Chain implement
+    # a lookup method with the same API as Simple backend does.
     class Chain < Base
       attr_accessor :backends
 
@@ -19,51 +33,6 @@ module I18n
         backends.map { |backend| backend.available_locales }.flatten.uniq
       end
 
-      # The implementation needs to be careful about the following I18n API
-      # features:
-      #
-      # * defaults: never pass any default option to the backends but instead
-      #   implement our own default mechanism (e.g. symbols as defaults would
-      #   need to be passed to the whole chain to be translated).
-      #
-      # * namespace lookup: only return if the result is not a hash OR count
-      #   is not present, otherwise merge them. So in effect the count variable
-      #   would control whether we have a namespace lookup or a pluralization
-      #   going on.
-      #
-      # * bulk translation: If the key is an array we need to call #translate
-      #   for each of the keys and collect the results.
-      #
-      # * exceptions: Make sure that we catch MissingTranslationData exceptions
-      #   and raise one in the end when no translation was found at all.
-      def translate(locale, key, options = {})
-        raise I18n::InvalidLocale.new(locale) if locale.nil?
-        return key.map { |key| translate(locale, key, options) } if key.is_a?(Array)
-
-        count, scope, default, separator = options.values_at(:count, *RESERVED_KEYS)
-        values = options.reject { |name, value| RESERVED_KEYS.include?(name) }
-        options.delete(:default)
-
-        entry = backends.inject({}) do |namespace, backend|
-          begin
-            translation = backend.translate(locale.to_sym, key, options)
-            if namespace_lookup?(translation, options)
-              namespace.merge!(translation) 
-            elsif !translation.nil?
-              return translation
-            end
-          rescue I18n::MissingTranslationData
-            namespace
-          end
-        end
-
-        entry = entry.nil? || entry.empty? ? default(locale, key, default, options) : resolve(locale, key, entry, options)
-        raise(I18n::MissingTranslationData.new(locale, key, options)) if entry.nil?
-        entry = pluralize(locale, entry, count)
-        entry = interpolate(locale, entry, values)
-        entry
-      end
-
       def localize(locale, object, format = :default)
         backends.each do |backend|
           result = backend.localize(locale, object, format) and return result
@@ -72,8 +41,18 @@ module I18n
 
       protected
 
-        def namespace_lookup?(entry, options)
-          entry.is_a?(Hash) and not options.has_key?(:count)
+        def lookup(locale, key, scope = [], separator = nil)
+          return unless key
+          result = {}
+          backends.each do |backend|
+            entry = backend.lookup(locale, key, scope, separator)
+            if entry.is_a?(Hash)
+              result.merge!(entry)
+            elsif !entry.nil?
+              return entry
+            end
+          end
+          result.empty? ? nil : result
         end
     end
   end
