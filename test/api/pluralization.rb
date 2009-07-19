c78ca1509befe3360367a80d7a362a6d755c3724
Sven Fuchs <svenfuchs@artweb-design.de>
2009-07-19 11:07:42 +0200

    make pluralization api tests independent of the implementation

--- test/api/pluralization.rb
+++ test/api/pluralization.rb
@@ -2,37 +2,32 @@ module Tests
   module Backend
     module Api
       module Pluralization
-        def test_pluralize_given_nil_returns_the_given_entry
-          entry = {:one => 'bar', :other => 'bars'}
-          assert_equal entry, I18n.backend.send(:pluralize, nil, entry, nil)
-        end
-
         def test_pluralize_given_0_returns_zero_string_if_zero_key_given
-          assert_equal 'zero', I18n.backend.send(:pluralize, nil, {:zero => 'zero', :one => 'bar', :other => 'bars'}, 0)
+          assert_equal 'zero', I18n.t(:default => { :zero => 'zero' }, :count => 0)
         end
 
         def test_pluralize_given_0_returns_plural_string_if_no_zero_key_given
-          assert_equal 'bars', I18n.backend.send(:pluralize, nil, {:one => 'bar', :other => 'bars'}, 0)
+          assert_equal 'bars', I18n.t(:default => { :other => 'bars' }, :count => 0)
         end
 
         def test_pluralize_given_1_returns_singular_string
-          assert_equal 'bar', I18n.backend.send(:pluralize, nil, {:one => 'bar', :other => 'bars'}, 1)
+          assert_equal 'bar', I18n.t(:default => { :one => 'bar' }, :count => 1)
         end
 
         def test_pluralize_given_2_returns_plural_string
-          assert_equal 'bars', I18n.backend.send(:pluralize, nil, {:one => 'bar', :other => 'bars'}, 2)
+          assert_equal 'bars', I18n.t(:default => { :other => 'bars' }, :count => 2)
         end
 
         def test_pluralize_given_3_returns_plural_string
-          assert_equal 'bars', I18n.backend.send(:pluralize, nil, {:one => 'bar', :other => 'bars'}, 3)
+          assert_equal 'bars', I18n.t(:default => { :other => 'bars' }, :count => 3)
+        end
+
+        def test_pluralize_given_nil_returns_the_given_entry
+          assert_equal({ :zero => 'zero' }, I18n.t(:default => { :zero => 'zero' }, :count => nil))
         end
 
         def test_interpolate_given_incomplete_pluralization_data_raises_invalid_pluralization_data
-          assert_raises(I18n::InvalidPluralizationData){ I18n.backend.send(:pluralize, nil, {:one => 'bar'}, 2) }
-        end
-
-        def test_pluralize_given_pluralization_data_as_default
-          assert_equal 'bars', I18n.t(:bar, :count => 2, :default => { :one => 'bar', :other => 'bars' })
+          assert_raises(I18n::InvalidPluralizationData){ I18n.t(:default => { :one => 'bar' }, :count => 2) }
         end
       end
     end
