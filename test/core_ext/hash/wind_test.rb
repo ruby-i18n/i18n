47153f330c68fa754cdec463cc8e7f5587437b49
Sven Fuchs <svenfuchs@artweb-design.de>
2009-08-04 00:07:05 +0200

    add tests for Hash#wind and Hash#unwind

---
+++ test/core_ext/hash/wind_test.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
+
+class HashWindTest < Test::Unit::TestCase
+  def test_unwind
+    hash = { "a" => { "b" => { "c" => "d", "e" => "f" }, "g" => "h" }, "i" => "j"}
+    expected = { "a.b.c" => "d", "a.b.e" => "f", "a.g" => "h", "i" => "j" }
+    assert_equal expected, hash.unwind
+  end
+
+  def test_wind
+    hash = { "a.b.c" => "d", "a.b.e" => "f", "a.g" => "h", "i" => "j" }
+    expected = { "a" => { "b" => { "c" => "d", "e" => "f" }, "g" => "h" }, "i" => "j"}
+    assert_equal expected, hash.wind
+  end
+end
