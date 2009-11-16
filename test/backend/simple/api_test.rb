3dcb269a2641039a12b6c64de3a64f587e112e8e
Sven Fuchs <svenfuchs@artweb-design.de>
2009-11-16 08:17:46 +0100

    fix implementation specific tests

--- test/backend/simple/api_test.rb
+++ test/backend/simple/api_test.rb
@@ -11,30 +11,32 @@ class I18nSimpleBackendApiBasicsTest < Test::Unit::TestCase
   end
 end
 
+require 'i18n/backend/fallbacks'
+
 class I18nSimpleBackendApiTranslateTest < Test::Unit::TestCase
   include Tests::Backend::Simple::Setup::Base
   include Tests::Backend::Api::Translation
 
   # implementation specific tests
-  
+
   def test_translate_calls_lookup_with_locale_given
-    I18n.backend.expects(:lookup).with('de', :bar, [:foo], nil).returns 'bar'
-    I18n.backend.translate 'de', :bar, :scope => [:foo]
+    I18n.backend.expects(:lookup).with(:de, :bar, [:foo], nil).returns 'bar'
+    I18n.backend.translate :de, :bar, :scope => [:foo]
   end
 
   def test_translate_calls_pluralize
-    I18n.backend.expects(:pluralize).with 'en', 'bar', 1
-    I18n.backend.translate 'en', :bar, :scope => [:foo], :count => 1
+    I18n.backend.expects(:pluralize).with(:en, 'bar', 1).returns 'bar'
+    I18n.backend.translate :en, :bar, :scope => [:foo], :count => 1
   end
 
   def test_translate_calls_interpolate
-    I18n.backend.expects(:interpolate).with 'en', 'bar', {}
-    I18n.backend.translate 'en', :bar, :scope => [:foo]
+    I18n.backend.expects(:interpolate).with(:en, 'bar', {}).returns 'bar'
+    I18n.backend.translate :en, :bar, :scope => [:foo]
   end
 
   def test_translate_calls_interpolate_including_count_as_a_value
-    I18n.backend.expects(:interpolate).with 'en', 'bar', {:count => 1}
-    I18n.backend.translate 'en', :bar, :scope => [:foo], :count => 1
+    I18n.backend.expects(:interpolate).with(:en, 'bar', {:count => 1}).returns 'bar'
+    I18n.backend.translate :en, :bar, :scope => [:foo], :count => 1
   end
 end
 
@@ -43,7 +45,7 @@ class I18nSimpleBackendApiInterpolateTest < Test::Unit::TestCase
   include Tests::Backend::Api::Interpolation
 
   # implementation specific tests
-  
+
   def test_interpolate_given_nil_as_a_string_returns_nil
     assert_nil I18n.backend.send(:interpolate, nil, nil, :name => 'David')
   end
