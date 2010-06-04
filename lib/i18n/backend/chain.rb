7180f06b69f49f8d860c4f433211403af9fd0542
Lawrence Pit <lawrence.pit@gmail.com>
2010-06-04 17:35:05 +1000

    Put Chain backend within a module Implementation so other modules like Fallbacks, Cache, etc. can be included.
    Signed-off-by: José Valim <jose.valim@gmail.com>

--- lib/i18n/backend/chain.rb
+++ lib/i18n/backend/chain.rb
@@ -17,61 +17,65 @@ module I18n
     # The implementation assumes that all backends added to the Chain implement
     # a lookup method with the same API as Simple backend does.
     class Chain
-      include Base
+      module Implementation
+        include Base
 
-      attr_accessor :backends
+        attr_accessor :backends
 
-      def initialize(*backends)
-        self.backends = backends
-      end
+        def initialize(*backends)
+          self.backends = backends
+        end
 
-      def reload!
-        backends.each { |backend| backend.reload! }
-      end
+        def reload!
+          backends.each { |backend| backend.reload! }
+        end
 
-      def store_translations(locale, data, options = {})
-        backends.first.store_translations(locale, data, options = {})
-      end
+        def store_translations(locale, data, options = {})
+          backends.first.store_translations(locale, data, options = {})
+        end
 
-      def available_locales
-        backends.map { |backend| backend.available_locales }.flatten.uniq
-      end
+        def available_locales
+          backends.map { |backend| backend.available_locales }.flatten.uniq
+        end
 
-      def translate(locale, key, options = {})
-        return key.map { |k| translate(locale, k, options) } if key.is_a?(Array)
+        def translate(locale, key, options = {})
+          return key.map { |k| translate(locale, k, options) } if key.is_a?(Array)
 
-        default = options.delete(:default)
-        namespace = {}
-        backends.each do |backend|
-          begin
-            options.update(:default => default) if default and backend == backends.last
-            translation = backend.translate(locale, key, options)
-            if namespace_lookup?(translation, options)
-              namespace.update(translation)
-            elsif !translation.nil?
-              return translation
+          default = options.delete(:default)
+          namespace = {}
+          backends.each do |backend|
+            begin
+              options.update(:default => default) if default and backend == backends.last
+              translation = backend.translate(locale, key, options)
+              if namespace_lookup?(translation, options)
+                namespace.update(translation)
+              elsif !translation.nil?
+                return translation
+              end
+            rescue MissingTranslationData
             end
-          rescue MissingTranslationData
           end
+          return namespace unless namespace.empty?
+          raise(I18n::MissingTranslationData.new(locale, key, options))
         end
-        return namespace unless namespace.empty?
-        raise(I18n::MissingTranslationData.new(locale, key, options))
+
+        def localize(locale, object, format = :default, options = {})
+          backends.each do |backend|
+            begin
+              result = backend.localize(locale, object, format, options) and return result
+            rescue MissingTranslationData
+            end
+          end
+          raise(I18n::MissingTranslationData.new(locale, format, options))
+        end
+
+        protected
+          def namespace_lookup?(result, options)
+            result.is_a?(Hash) and not options.has_key?(:count)
+          end
       end
 
-      def localize(locale, object, format = :default, options = {})
-        backends.each do |backend|
-          begin
-            result = backend.localize(locale, object, format, options) and return result
-          rescue MissingTranslationData
-          end
-        end
-        raise(I18n::MissingTranslationData.new(locale, format, options))
-      end
-
-      protected
-        def namespace_lookup?(result, options)
-          result.is_a?(Hash) and not options.has_key?(:count)
-        end
+      include Implementation
     end
   end
 end
