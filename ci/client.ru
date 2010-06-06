6c10052d9d9d14e4f75b3a20fa99ea1c914f92d9
Sven Fuchs <svenfuchs@artweb-design.de>
2010-06-06 23:46:22 +0200

    move ci to a separate branch

--- ci/client.ru
+++
@@ -1,4 +0,0 @@
-require 'rubygems'
-require 'ci/controllers/client'
-
-run Ci::Client.new
--- ci/setup.rb
+++
@@ -1,4 +0,0 @@
-require 'ci/heroku'
-
-Heroku.server('i18n')
-Heroku.each_stack { |stack| Heroku.client('i18n', stack) }
