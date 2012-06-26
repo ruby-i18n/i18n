a5b96d30fa8f2d57fe9f593a3fdcc8db02b4e19f
Christopher Dell <chris@tigrish.com>
2012-06-26 16:02:48 +0200

    Strip reserved keys from interpolations when a Translation is initialized

--- lib/i18n/backend/translation.rb
+++ lib/i18n/backend/translation.rb
@@ -1,16 +1,16 @@
 module I18n
   module Backend
     class Translation
-      attr_accessor :locale, :key, :content, :scope, :count, :default, :options
+      attr_accessor :locale, :key, :content, :scope, :count, :default, :interpolations
 
       def initialize(args)
-        @locale  = args.delete :locale
-        @key     = args.delete :key
-        @scope   = args.delete :scope
-        @count   = args.delete :count
-        @default = args.delete :default
-        @options = args || {}
-        @content = nil
+        @locale         = args.delete :locale
+        @key            = args.delete :key
+        @scope          = args.delete :scope
+        @count          = args.delete :count
+        @default        = args.delete :default
+        @interpolations = args.except(*RESERVED_KEYS)
+        @content        = nil
       end
     end
   end
--- test/backend/translation_test.rb
+++ test/backend/translation_test.rb
@@ -18,7 +18,14 @@ class I18nBackendTranslationTest < Test::Unit::TestCase
     assert_equal('foo.bar', translation.key)
     assert_equal('baz.biz', translation.scope)
     assert_equal(2, translation.count)
-    assert_equal({:custom_1 => 'interpolation 1', :custom_2 => 'interpolation 2'}, translation.options)
+    assert_equal({:custom_1 => 'interpolation 1', :custom_2 => 'interpolation 2'}, translation.interpolations)
     assert_nil(translation.content)
+  end
+
+  test "it strips RESERVED_KEYS from interpolations upon initialization" do
+    reserved_keys_hash = {}
+    I18n::RESERVED_KEYS.each {|key| reserved_keys_hash[key] = key }
+    translation = I18n::Backend::Translation.new(attributes.merge(reserved_keys_hash))
+    assert_equal({:custom_1 => 'interpolation 1', :custom_2 => 'interpolation 2'}, translation.interpolations)
   end
 end
