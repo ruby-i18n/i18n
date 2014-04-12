6f291c427bd2df08037fbc500c529fa71b167f97
Jimmy Cuadra <jimmy@jimmycuadra.com>
2014-04-11 21:01:24 -0700

    Maintain subclasses of Hash when calling Hash#slice.
    Fixes #250

--- lib/i18n/core_ext/hash.rb
+++ lib/i18n/core_ext/hash.rb
@@ -1,6 +1,6 @@
 class Hash
   def slice(*keep_keys)
-    h = {}
+    h = self.class.new
     keep_keys.each { |key| h[key] = fetch(key) if has_key?(key) }
     h
   end unless Hash.method_defined?(:slice)
@@ -21,7 +21,7 @@ class Hash
   MERGER = proc do |key, v1, v2|
     Hash === v1 && Hash === v2 ? v1.merge(v2, &MERGER) : v2
   end
-  
+
   def deep_merge!(data)
     merge!(data, &MERGER)
   end unless Hash.method_defined?(:deep_merge!)
