38f7d53209207613f61e77278282185807e50758
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-05-09 10:09:13 -0300

    Use bundle exec when running tests for all gemfiles
    Refactor script a bit, improve output.

--- test/run_all.rb
+++ test/run_all.rb
@@ -2,20 +2,19 @@ def bundle_check
   `bundle check` == "Resolving dependencies...\nThe Gemfile's dependencies are satisfied\n"
 end
 
-command  = 'ruby -w -Ilib -Itest test/all.rb'
-gemfiles = %w(Gemfile) + Dir['gemfiles/Gemfile*'].reject { |f| f.end_with?('.lock') }
-
-results = gemfiles.map do |gemfile|
-  puts "BUNDLE_GEMFILE=#{gemfile}"
-  ENV['BUNDLE_GEMFILE'] = File.expand_path("../../#{gemfile}", __FILE__)
-
-  unless bundle_check
-    puts "bundle install"
-    system('bundle install')
-  end
-
+def execute(command)
   puts command
   system command
 end
 
-exit(results.inject(true) { |a, b| a && b })
+gemfiles = %w(Gemfile) + Dir['gemfiles/Gemfile*'].reject { |f| f.end_with?('.lock') }
+
+results = gemfiles.map do |gemfile|
+  puts "\nBUNDLE_GEMFILE=#{gemfile}"
+  ENV['BUNDLE_GEMFILE'] = File.expand_path("../../#{gemfile}", __FILE__)
+
+  execute 'bundle install' unless bundle_check
+  execute 'bundle exec ruby -w -Ilib -Itest test/all.rb'
+end
+
+exit results.all?
