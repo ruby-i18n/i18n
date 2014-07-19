b20ac334d4d38b2fd0342c86757c1daaf8c173ea
Carlos Antonio da Silva <carlosantoniodasilva@gmail.com>
2014-07-19 11:12:52 -0300

    Remove old "gem" hack for Rails 2.3 tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,15 +19,6 @@ rescue LoadError
   TEST_CASE = Test::Unit::TestCase
 end
 
-# Do not load the i18n gem from libraries like active_support.
-#
-# This is required for testing against Rails 2.3 because active_support/vendor.rb#24 tries
-# to load I18n using the gem method. Instead, we want to test the local library of course.
-alias :gem_for_ruby_19 :gem # for 1.9. gives a super ugly seg fault otherwise
-def gem(gem_name, *version_requirements)
-  gem_name =='i18n' ? puts("skipping loading the i18n gem ...") : super
-end
-
 require 'bundler/setup'
 require 'i18n'
 require 'mocha/setup'
