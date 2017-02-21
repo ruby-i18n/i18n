ruby-i18n/i18n

commit f6a4294c2cd8b2e8a285b8a394819e46851fa2b0

Set HashTransliterator default replacement char only once
If a string is 1000 characters longs, the previous code would check
1000 times if the `replacement` argument was nil, then fallback to
`DEFAULT_REPLACEMENT_CHAR`

By setting in the method's argument definition the default value
once, we skip this potentiel unnecessary check.

lib/i18n/backend/transliterator.rb
@@ -75,9 +75,9 @@ module I18n
           add rule if rule
         end
 
-        def transliterate(string, replacement = nil)
+        def transliterate(string, replacement = DEFAULT_REPLACEMENT_CHAR)
           string.gsub(/[^\x00-\x7f]/u) do |char|
-            approximations[char] || replacement || DEFAULT_REPLACEMENT_CHAR
+            approximations[char] || replacement
           end
         end
 

commit 6b60a5b523de4015842d43dff8255c360ff51dec

Fix transliteration to default replacement char
Commit f6a4294c introduced a bug where non-ASCII chars not present in
the transliteration map would be removed from the output, instead of
being replaced by the default replacement char, violating the
behavior stated in the documentation:

    I18n.transliterate("日本語")
    # => ""         (should be "???")

This fixes the issue while preserving the optimization introduced by
that commit.

lib/i18n/backend/transliterator.rb
@@ -75,7 +75,8 @@ module I18n
           add rule if rule
         end
 
-        def transliterate(string, replacement = DEFAULT_REPLACEMENT_CHAR)
+        def transliterate(string, replacement = nil)
+          replacement ||= DEFAULT_REPLACEMENT_CHAR
           string.gsub(/[^\x00-\x7f]/u) do |char|
             approximations[char] || replacement
           end

test/i18n_test.rb
@@ -351,6 +351,10 @@ class I18nTest < I18n::TestCase
     end
   end
 
+  test "transliterate non-ASCII chars not in map with default replacement char" do
+    assert_equal "???", I18n.transliterate("日本語")
+  end
+
   test "I18n.locale_available? returns true when the passed locale is available" do
     I18n.available_locales = [:en, :de]
     assert_equal true, I18n.locale_available?(:de)